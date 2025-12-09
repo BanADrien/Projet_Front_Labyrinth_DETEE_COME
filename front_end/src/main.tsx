1fa40ae516efdf927367e6835aa1cfc5b18d2231
Tankiste07 <remi007tank@gmail.com>
2025-12-09 15:57:05 +0100

    page home focntionnel

--- front_end/src/main.tsx
+++ front_end/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
-import Home from "./home"; // <-- page d'accueil
+import Home from "./Home"; // <-- page d'accueil
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
